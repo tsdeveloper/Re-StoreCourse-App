c9d88c9445751bf086729a1924aa1f65f5891f41
Thyago Developer <ts.analista@gmail.com>
2024-12-19 22:38:59 -0300

    feat: add product card. add card mui. add avatar product. add dark mode theme change website.

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,12 +1,25 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
 
-export default function Header() {
+interface Props {
+  darkMode: boolean;
+  handleThemeChange: () => void;
+}
+
+export default function Header({darkMode, handleThemeChange}: Props) {
+
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ mb: 4 }}>
       <Toolbar>
-      <Typography variant='h6'>Re-StoreCourse App</Typography>
+        <Typography variant="h6">Re-StoreCourse App</Typography>
+        <Typography sx={{m: 'auto auto' }}>
+          Theme Dark
+          <Switch
+            checked={darkMode}
+            onChange={handleThemeChange}
+            inputProps={{ "aria-label": "controlled" }}
+          />
+        </Typography>
       </Toolbar>
-
     </AppBar>
-  )
+  );
 }
--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -1,30 +1,63 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import { Product } from "../../app/models/product";
+import { deepOrange } from "@mui/material/colors";
 
 interface Props {
   product: Product;
 }
-export default function ProductCard({product}: Props) {
+
+export default function ProductCard({ product }: Props) {
+  console.log(product);
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
-    <CardMedia
-      sx={{ height: 140 }}
-      image={product.pictureUrl}
-      title="green iguana"
-    />
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        Lizard
-      </Typography>
-      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-        Lizards are a widespread group of squamate reptiles, with over 6,000
-        species, ranging across all continents except Antarctica
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Share</Button>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </Card>
+    <Card>
+      <CardHeader
+        avatar={
+          // <Avatar
+          // src={product.pictureUrl}>
+          //   {product.name.charAt(0).toUpperCase()}
+          // </Avatar>
+          <Avatar sx={{ bgcolor: deepOrange[300]}}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
+        title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: "bold", color: "primary.main" },
+          variant: "h5"
+        }}
+        subheader={
+          <Typography variant="subtitle2">          
+            {product.brand?.name}/{product.type?.name}
+          </Typography>
+        }
+      />
+      <CardMedia
+        sx={{ height: 140, backgroundSize: "cover", bgcolor: 'primary.contrastText' }}
+        image={product.pictureUrl}
+        title={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom color="secondary" variant="h5">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to cart</Button>
+        <Button size="small">View</Button>
+      </CardActions>
+    </Card>
   );
 }
