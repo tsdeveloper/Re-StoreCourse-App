338b4cf42ec1077091fe8c15669d60f5d9bfff62
Thyago Developer <ts.analista@gmail.com>
2025-01-16 22:11:13 -0300

    feat: add component loading

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,19 +1,24 @@
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 
-
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-  agent.Catalog.list().then(products => setProducts(products))
+    agent.Catalog.list().then((products) => setProducts(products));
   }, []);
 
+  console.log(products);
+
+  if (loading) return <LoadingComponent />;
   return (
     <>
-     <ProductList products={products} />
+      <ProductList products={products} />
     </>
   );
 }
