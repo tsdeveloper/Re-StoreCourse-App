1ce1f455956a55d8bb8809bef29cecdf9e5af1f9
Thyago Developer <ts.analista@gmail.com>
2025-02-23 09:20:52 -0300

    feat: get basket by API

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCart } from '@mui/icons-material'
+import { ShoppingCart } from "@mui/icons-material";
 import {
   AppBar,
   Badge,
@@ -9,59 +9,59 @@ import {
   Switch,
   Toolbar,
   Typography,
-} from '@mui/material'
-import { NavLink } from 'react-router'
+} from "@mui/material";
+import { Link, NavLink } from "react-router";
 
 const midLinks = [
-  { title: 'catalog', path: '/catalog' },
-  { title: 'about', path: '/about' },
-  { title: 'contact', path: '/contact' },
-]
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
 
 const rightLinks = [
-  { title: 'login', path: '/login' },
-  { title: 'register', path: '/register' },
-]
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
 
 interface Props {
-  darkMode: boolean
-  handleThemeChange: () => void
+  darkMode: boolean;
+  handleThemeChange: () => void;
 }
 
 const navStyles = {
-  color: 'inherit',
-  typography: 'h6',
-  textDecoration: 'none',
-  '&:hover': {
-    color: 'grey.500',
+  color: "inherit",
+  typography: "h6",
+  textDecoration: "none",
+  "&:hover": {
+    color: "grey.500",
   },
-  '&.active': {
-    color: 'text.secondary',
+  "&.active": {
+    color: "text.secondary",
   },
-}
+};
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
   return (
-    <AppBar position='static' sx={{ mb: 4 }}>
+    <AppBar position="static" sx={{ mb: 4 }}>
       <Toolbar
         sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
-        <Box display='flex' alignItems='center'>
-          <Typography variant='h6' component={NavLink} to='/' sx={navStyles}>
+        <Box display="flex" alignItems="center">
+          <Typography variant="h6" component={NavLink} to="/" sx={navStyles}>
             Re-StoreCourse App
           </Typography>
           <Switch
             checked={darkMode}
             onChange={handleThemeChange}
-            inputProps={{ 'aria-label': 'controlled' }}
+            inputProps={{ "aria-label": "controlled" }}
           />
         </Box>
 
-        <List sx={{ display: 'flex' }}>
+        <List sx={{ display: "flex" }}>
           {midLinks.map(({ title, path }) => (
             <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
               {title.toUpperCase()}
@@ -69,14 +69,21 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
           ))}
         </List>
 
-        <Box display='flex' alignItems='center'>
-          <IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
-            <Badge badgeContent='4' color='secondary'>
+        <Box display="flex" alignItems="center">
+          <IconButton
+            component={Link}
+            to="/basket"
+            size="large"
+            edge="start"
+            color="inherit"
+            sx={{ mr: 2 }}
+          >
+            <Badge badgeContent="4" color="secondary">
               <ShoppingCart />
             </Badge>
           </IconButton>
 
-          <List sx={{ display: 'flex' }}>
+          <List sx={{ display: "flex" }}>
             {rightLinks.map(({ title, path }) => (
               <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
                 {title.toUpperCase()}
@@ -86,5 +93,5 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
         </Box>
       </Toolbar>
     </AppBar>
-  )
+  );
 }
