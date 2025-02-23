2a775f9c3713293f8ee17e9af155e949910a277d
Thyago Developer <ts.analista@gmail.com>
2025-02-23 09:20:30 -0300

    feat: fix price product page.

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,43 +1,54 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import { Product } from '../../app/models/product'
-import Grid from '@mui/material/Grid2'
-import { Divider, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material'
-import agent from '../../app/api/agent'
-import LoadingComponent from '../../app/layout/LoadingComponent'
-import NotFound from '../../app/errors/NotFound'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Product } from "../../app/models/product";
+import Grid from "@mui/material/Grid2";
+import {
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import NotFound from "../../app/errors/NotFound";
 
 export default function ProductDetails() {
-  const { id } = useParams<{ id: string }>()
-  const [product, setProduct] = useState<Product | null>(null)
-  const [loading, setLoading] = useState(true)
+  const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    id && agent.Catalog.details(parseInt(id))
-      .then((response) => setProduct(response))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false))
-  }, [id])
+    id &&
+      agent.Catalog.details(parseInt(id))
+        .then((response) => setProduct(response))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+  }, [id]);
 
-  console.log(product)
+  console.log(product);
 
-  if (loading) return <LoadingComponent message='Loading product...' />
+  if (loading) return <LoadingComponent message="Loading product..." />;
 
-  if (!product) return <NotFound />
+  if (!product) return <NotFound />;
   return (
     <Grid container spacing={6}>
       <Grid size={{ xs: 6 }}>
         <img
           src={product.pictureUrl}
           alt={product.name}
-          style={{ width: '100%' }}
+          style={{ width: "100%" }}
         />
       </Grid>
       <Grid size={{ xs: 6 }}>
-        <Typography variant='h3'>{product.name}</Typography>
-        <Divider sx={{ mb: 2}} />
-        <Typography variant='h4' color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">
+          ${product.price.toFixed(2)}
+        </Typography>
         <TableContainer>
           <Table>
             <TableBody>
@@ -66,5 +77,5 @@ export default function ProductDetails() {
         </TableContainer>
       </Grid>
     </Grid>
-  )
+  );
 }
