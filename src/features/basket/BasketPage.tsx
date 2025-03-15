eaf6abeb6b55c723697dc51a4bb44ba987eb21bb
Thiago Almeida <ts.analista@gmail.com>
2025-03-14 23:06:20 -0300

    feat: add remove and add item basket page.

--- src/features/basket/BasketPage.tsx
+++ src/features/basket/BasketPage.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Basket } from '../../app/models/basket';
-import agent from '../../app/api/agent';
-import LoadingComponent from '../../app/layout/LoadingComponent';
 import {
+  Box,
   IconButton,
   Paper,
   Table,
@@ -14,19 +13,32 @@ import {
   Typography,
 } from '@mui/material';
 import Delete from '@mui/icons-material/Delete';
+import { Add, Remove } from '@mui/icons-material';
+import { useStoreContext } from '../../app/context/StoreContext';
+import agent from '../../app/api/agent';
+import { LoadingButton } from '@mui/lab';
 
 export function BasketPage() {
-  const [loading, setLoading] = useState(true);
-  const [basket, setBasket] = useState<Basket | null>(null);
+  const { basket, setBasket, removeItem } = useStoreContext();
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    agent.Basket.get()
+  function handleAddItem(productId: number) {
+    setLoading(true);
+    agent.Basket.addItem(productId, 1)
       .then((basket) => setBasket(basket))
+      .then(() => console.log('Item added to basket'))
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
-  }, []);
+  }
 
-  if (loading) return <LoadingComponent message="Loading basket..." />;
+  function handleRemoveItem(productId: number, quantity = 1) {
+    setLoading(true);
+    agent.Basket.removeItem(productId, quantity)
+      .then(() => removeItem(productId, quantity))
+      .then(() => console.log('Item removed from basket'))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
 
   if (!basket) return <Typography variant="h3">Basket is empty</Typography>;
 
@@ -52,19 +64,46 @@ export function BasketPage() {
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {item.name}
+                  <Box display={'flex'} alignItems={'center'}>
+                    <img
+                      src={item.pictureUrl}
+                      alt={item.name}
+                      style={{ height: 50, marginRight: 20 }}
+                    />
+                    <span>{item.name}</span>
+                  </Box>
                 </TableCell>
                 <TableCell align="right" component="th" scope="row">
                   ${item.price.toFixed(2)}
                 </TableCell>
-                <TableCell align="right">{item.quantity}</TableCell>
+                <TableCell align="right">
+                  <LoadingButton
+                    loading={loading}
+                    color="error"
+                    onClick={() => handleRemoveItem(item.productId)}
+                  >
+                    <Remove />
+                  </LoadingButton>
+                  {item.quantity}
+                  <LoadingButton
+                    color="secondary"
+                    loading={loading}
+                    onClick={() => handleAddItem(item.productId)}
+                  >
+                    <Add />
+                  </LoadingButton>
+                </TableCell>
                 <TableCell align="right">
                   ${(item.quantity * item.price).toFixed(2)}
                 </TableCell>
                 <TableCell align="right">
-                  <IconButton color="error">
+                  <LoadingButton
+                    color="error"
+                    onClick={() => handleRemoveItem(item.productId)}
+                    loading={loading}
+                  >
                     <Delete />
-                  </IconButton>
+                  </LoadingButton>
                 </TableCell>
               </TableRow>
             ))}
