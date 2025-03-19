297300dad9eec87932460c02969c4455e3ef4a1e
Thiago Almeida <ts.analista@gmail.com>
2025-03-19 08:27:18 -0300

    feat: add method set currrency format

--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -2,3 +2,14 @@ export function getCookie(key: string) {
   const b = document.cookie.match('(^|;)\\s*' + key + '\\s*=\\s*([^;]+)');
   return b ? b.pop() : '';
 }
+
+export function currencyFormat(
+  amount: number,
+  locales = 'pt-BR',
+  currency = 'BRL',
+) {
+  return new Intl.NumberFormat(locales, {
+    style: 'currency',
+    currency: currency,
+  }).format(amount);
+}
