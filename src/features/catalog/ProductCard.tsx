61964cd48e1bd8ac0732825aec2b46ca9d119673
Thiago Almeida <ts.analista@gmail.com>
2025-07-28 23:01:14 -0300

    feat: resolve async log redux thunk with Redux DevTools

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -11,29 +11,18 @@ import {
 import { Product } from '../../app/models/product';
 import { deepOrange } from '@mui/material/colors';
 import { Link } from 'react-router';
-import agent from '../../app/api/agent';
-import { useState } from 'react';
 import { LoadingButton } from '@mui/lab';
 import { currencyFormat } from '../../app/util/util';
-import { useAppDispatch } from '../../app/store/configureStore';
-import { setBasket } from '../basket/basketSlice';
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
+import { addBasketItemAsync } from '../basket/basketSlice';
 
 interface Props {
   product: Product;
 }
 
 export default function ProductCard({ product }: Props) {
-  const [loading, setLoading] = useState(false);
+  const { status } = useAppSelector((state) => state.basket);
   const dispatch = useAppDispatch();
-
-  function handleAddItem(productId: number) {
-    setLoading(true);
-    agent.Basket.addItem(productId, 1)
-      .then((basket) => dispatch(setBasket(basket)))
-      .then(() => console.log('Item added to basket'))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
-  }
 
   return (
     <Card>
@@ -77,8 +66,10 @@ export default function ProductCard({ product }: Props) {
       </CardContent>
       <CardActions>
         <LoadingButton
-          loading={loading}
-          onClick={() => handleAddItem(product.id)}
+          loading={status.includes('pending')}
+          onClick={() =>
+            dispatch(addBasketItemAsync({ productId: product.id }))
+          }
           size="small"
         >
           Add to Cart
