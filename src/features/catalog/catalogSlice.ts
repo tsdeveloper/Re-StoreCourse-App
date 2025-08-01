98fb6eb28d09d34e062f64a598254627b480b2e9
Thyago Developer <ts.analista@gmail.com>
2025-08-01 00:33:17 -0300

    feat: resolve buggy error .NET Server

---
+++ src/features/catalog/catalogSlice.ts
@@ -0,0 +1,79 @@
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from '@reduxjs/toolkit';
+import { Product } from '../../app/models/product';
+import agent from '../../app/api/agent';
+import { RootState } from '../../app/store/configureStore';
+
+const productsAdapter = createEntityAdapter<Product>();
+
+export const fetchProductsAsync = createAsyncThunk<Product[]>(
+  'catalog/fetchProductsAsync',
+  async (_, thunkAPI) => {
+    try {
+      const response = await agent.Catalog.list();
+      return response;
+    } catch (error: any) {
+      console.error('Failed to fetch products:', error);
+      return thunkAPI.rejectWithValue({error: error.data})
+    }
+  },
+);
+
+export const fetchProductAsync = createAsyncThunk<Product, number>(
+  'catalog/fetchProductAsync',
+  async (productId, thunkAPI) => {
+    try {
+      const response = await agent.Catalog.details(productId);
+      return response;
+    } catch (error: any) {
+      console.error('Failed to fetch products:', error);
+      return thunkAPI.rejectWithValue({error: error.data});
+    }
+  },
+);
+
+export const catalogSlice = createSlice({
+  name: 'catalog',
+  initialState: productsAdapter.getInitialState({
+    productsLoaded: false,
+    status: 'idle',
+  }),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProductsAsync.pending, (state) => {
+      state.status = 'pendingFetchProducts';
+    });
+
+    builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
+      productsAdapter.setAll(state, action.payload);
+      state.status = 'idle';
+      state.productsLoaded = true;
+    });
+
+    builder.addCase(fetchProductsAsync.rejected, (state, action) => {
+      console.log(action.payload);
+      state.status = 'idle';
+    });
+
+    builder.addCase(fetchProductAsync.pending, (state) => {
+      state.status = 'pendingFetchProduct';
+    });
+
+    builder.addCase(fetchProductAsync.fulfilled, (state, action) => {
+      productsAdapter.upsertOne(state, action.payload);
+      state.status = 'idle';
+    });
+
+    builder.addCase(fetchProductAsync.rejected, (state, action) => {
+      console.log(action);
+      state.status = 'idle';
+    });
+  },
+});
+
+export const productSelectors = productsAdapter.getSelectors(
+  (state: RootState) => state.catalog,
+);
