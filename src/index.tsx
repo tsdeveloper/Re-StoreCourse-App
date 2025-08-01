71b4178c961d9a2b7ff1f031ac39be2a2a625dc7
Thiago Almeida <ts.analista@gmail.com>
2025-07-31 23:13:53 -0300

    feat: add redux thunk async

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router';
 import { router } from './app/router/Routes';
-import { StoreProvider } from './app/context/StoreContext';
 import { Provider } from 'react-redux';
 import { store } from './app/store/configureStore';
 
